3cbcdbf96979d8363907e3c19c3759de6e5d5f82
jacopo-derrico <derricoj.design@gmail.com>
2024-10-29 10:50:45 +0100

    feat: api call and products storage

--- app.vue
+++ app.vue
@@ -1,6 +1,28 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+
+    <NuxtPage/>
   </div>
 </template>
+
+<script setup>
+  import { useProductsStore } from '~/store/productsApi';
+
+  const allProductsStore = useProductsStore();
+  const allProducts = computed(() => allProductsStore.allProducts);
+
+  const fetchProducts = async () => {
+      try {
+          const response = await fetch('https://fakestoreapi.com/products')
+          const data = await response.json()
+          allProducts.value.push(...data)
+          // console.log(allProducts)
+      } catch (error) {
+          console.error('Error fetching products:', error)
+      }
+  }
+
+  fetchProducts()
+
+</script>
