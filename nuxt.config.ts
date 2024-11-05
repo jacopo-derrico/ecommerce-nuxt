a5e52a1e87857c0fdb752f33b60bf4a1ec9d9ae3
jacopo-derrico <derricoj.design@gmail.com>
2024-11-05 17:26:51 +0100

    fix: deactivated devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ const getProductRoutes = async () => {
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: [
     '@pinia/nuxt',
     'nuxt-quasar-ui',
