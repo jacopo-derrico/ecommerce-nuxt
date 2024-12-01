5d85b78f460013c967ceedff9f6a5fc0c844c18c
jacopo-derrico <derricoj.design@gmail.com>
2024-12-01 16:57:07 +0100

    style: change grid and responsiveness

--- components/AppProducts.vue
+++ components/AppProducts.vue
@@ -1,33 +1,39 @@
 <template>
-    <div>
-        <div class="flex w-11/12 mx-auto justify-between">
-            <q-card
-                class="my-card m-1 w-1/6 h-[500px] flex flex-col justify-between p-2 hover:shadow-blue-700 hover:shadow-md"
-                v-for="product in productsStore.productsList" :key="product.name" :product="product">
-                <figure class="h-[250px] w-fit self-center overflow-hidden">
-                    <img class="w-full h-[250px]" :src="product.image"></img>
+        <div class="grid grid-cols-2 md:grid-cols-4 gap-4 my-20">
+            <NuxtLink
+                class="my-card rounded-lg shadow-md flex flex-col justify-between border-2 border-white hover:border-blue-700 hover:border-2"
+                v-for="product in productsStore.productsList" :key="product.name" :product="product"
+                :to="{ name: 'products-id', params: { id: product.id } }">
+                
+                <figure class="aspect-square flex items-center justify-center overflow-hidden p-4">
+                    <img class="h-full rounded-s-lg rounded-e-lg" :src="product.image"></img>
                 </figure>
 
-                <q-card-section class="q-pt-none q-pb-none">
+                <div class="">
                     <NuxtLink :to="{ name: 'products-id', params: { id: product.id } }">
                         <div class="text-lg font-bold">{{ product.title }}</div>
                     </NuxtLink>
-                    <q-badge color="primary" text-color="white" icon="event">
+                    <span >
                         {{ product.category }}
-                    </q-badge>
-                </q-card-section>
+                    </span>
+                </div>
 
-                <q-card-section class="q-pt-none">
+                <div class="mt-2">
                     {{ product.price }}€
-                </q-card-section>
+                </div>
 
-                <q-card-section class="q-pt-none  q-gutter-md">
-                    <q-btn color="primary" label="Add to cart" @click="addToCart(product)" />
-                    <q-btn color="primary" label="<3" @click="addToWishlist(product)" />
-                </q-card-section>
-            </q-card>
+                <div class="md-2 flex justify-center items-center">
+                    <button class="w-full text-center" @click="addToCart(product)">Add to cart</button>
+                    <button class="w-full text-center" @click="addToWishlist(product)">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                        </svg>
+                    </button>
+                </div>
+            </NuxtLink>
         </div>
-    </div>
 </template>
 
 <script setup>
@@ -68,8 +74,8 @@
 </script>
 
 <style scoped>
-    .my-card:hover img {
+    /* .my-card:hover img {
         transform: scale(1.1);
         transition: transform 330ms ease-in-out;
-    }
+    } */
 </style>
